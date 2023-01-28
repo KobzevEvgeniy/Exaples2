621aa1a5da5eb3affc639bce2879c1f9554a139f
Evgeniy <public-judge@yandex.ru>
2023-01-28 18:12:52 +0300

    add any number

--- Exemple004_Div/Program.cs
+++ Exemple004_Div/Program.cs
@@ -1,5 +1,5 @@
 ﻿// программа определяет числа и делит одно на другое
-double numberA=12;
-double numberB=5;
+double numberA=10;
+double numberB=2;
 double result=numberA/numberB;
 Console.WriteLine(result);
