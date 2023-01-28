1ebb4ecb28e60cc42fea85a28df00b1070b65ec2
Evgeniy <public-judge@yandex.ru>
2023-01-28 14:16:34 +0300

    Программа ищет число в массиве ,если оно совпадает то выводит его индекс

---
+++ Exemple010_IntroArray/Program.cs
@@ -0,0 +1,16 @@
+﻿// ищет в массиве число совпадающее с введенным и говорит нашел или нет
+int [] array = {1, 12, 34, 44, 54, 64, 44, 74, 84};
+
+int n= array.Length;
+int find=44;
+int index=0;
+
+while (index<n)
+{
+    if (array [index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+        }
+    index++;
+}
