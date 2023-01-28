c77e377feeb08026a1b008b53ec22ae52d22793e
Evgeniy <public-judge@yandex.ru>
2023-01-28 13:58:37 +0300

    Третий вариант с использованием массива и метода

---
+++ Exemple009_IntroArray/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Третий вариант решения с помошью массива");
+int Max(int arg1, int arg2, int arg3)
+{
+    int result =arg1;
+    if (arg2>result) result= arg2;
+    if (arg3>result) result= arg3;
+    return result;
+}
+int [] array ={11,211,31,41,52,61,78,18,19};
+int maximum=Max(
+    Max(array[0],array[1],array[2]),
+    Max(array[3],array[4],array[5]),
+    Max(array[6],array[7],array[8])
+);
+Console.Write($"Максимальное число из девяти предложенных- {maximum} ");
