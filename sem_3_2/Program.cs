538d308aa7642f398a31c5c98883901f8d3f9faa
Evgeniy <public-judge@yandex.ru>
2023-02-02 22:35:05 +0300

    seminer 3

---
+++ sem_3_2/Program.cs
@@ -0,0 +1,30 @@
+﻿// **Задача 18:**
+
+//Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y)
+Console.Clear();
+Console.Write("Задайте номер четверти: ");
+int part = int.Parse(Console.ReadLine());
+
+if(part==1)
+{
+    Console.WriteLine("координаты расположены в диапозоне X>0 Y>0");
+}
+if(part==2)
+{
+    Console.WriteLine("координаты расположены в диапозоне X<0 Y>0");
+}
+if(part==3)
+{
+    Console.WriteLine("координаты расположены в диапозоне X<0 Y<0");
+}
+if(part==4)
+{
+    Console.WriteLine("координаты расположены в диапозонеX>0 Y<0");
+}
+if (part== 0)
+    Console.WriteLine("На оси X == 0 || Y == 0");
+if (part>5)
+{
+    Console.WriteLine("задано не верно число");
+}
+
---
+++ sem_3_3/Program.cs
@@ -0,0 +1,22 @@
+﻿// // Напишите программу, 
+// которая принимает на вход координаты двух точек и находит 
+// расстояние между ними в 2D пространстве.
+
+// A (3,6); B(2, 1)-> 5,09
+// A(7, -5); B(1, -1)-> 7,21
+//sqrt((3 - 2)^2 + (6 - 1)^2) = 5.09
+///double s = Math.Sqrt(...);
+Console.Clear();
+Console.Write("Введите координаты для первой точки X: ");
+int X = int.Parse(Console.ReadLine());
+
+Console.Write("Введите координаты для первой точки Y: ");
+int Y = int.Parse(Console.ReadLine());
+
+Console.Write("Введите координаты для второй точки X: ");
+int B = int.Parse(Console.ReadLine());
+
+Console.Write("Введите координаты для второй точки Y: ");
+int Z = int.Parse(Console.ReadLine());
+double result = Math.Sqrt(Math.Pow(X-B,2)+Math.Pow(Y-Z,2));
+Console.Write($"Расстояние между точками result= {result:F3}");
