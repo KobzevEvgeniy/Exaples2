8a6ff35e9f1f5d1441ee8488843ff006004c7182
Evgeniy <public-judge@yandex.ru>
2023-02-04 15:16:01 +0300

    таблица умножения и цикл for

---
+++ lection_3_metod_1234/Program.cs
@@ -0,0 +1,14 @@
+﻿// Использование цикла форм для создание таблицы умножения
+Console.WriteLine("Таблица умножения");
+for (int i = 2; i <= 10; i++ )
+{
+    for (int J = 2; J <= 10; J++ )
+    {
+        Console.WriteLine($"{i} * {J} = {i*J}");
+    }
+    Console.WriteLine();
+}
+
+
+
+
