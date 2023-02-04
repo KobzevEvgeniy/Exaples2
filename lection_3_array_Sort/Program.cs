ba21659ed4b26f01e0fa53791d997ac813ac4fc5
Evgeniy <public-judge@yandex.ru>
2023-02-04 15:11:48 +0300

    сортировка массива от минимального к максимальному числу

---
+++ lection_3_array_Sort/Program.cs
@@ -0,0 +1,33 @@
+﻿// Данн массив чисел.
+//Надо найти позицию минимального числа в неотсортированной части массива
+//Произвести обмен этого значения со значением из первой позиции 
+// повторять пока есть элементы неотсортированные 
+int [] array = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+void PrintArray(int [] array)
+{
+    int count = array.Length;
+    for (int i=0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort (int [] array)
+{
+    for (int i=0; i < array.Length -1; i++)
+    {
+        int minPosition = i;
+            for(int j= i +1 ; j< array.Length; j++)
+            {
+                if (array [j]< array [minPosition]) minPosition =j;
+            }
+        int temperary = array [i];
+        array [i] = array [minPosition];
+        array [minPosition] = temperary;
+    }
+}
+PrintArray(array);
+SelectionSort(array);
+PrintArray(array);
+
