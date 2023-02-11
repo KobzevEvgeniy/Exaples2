b1f41d7f37b1fc2b2d988d7dcc41c423ebe6d012
Evgeniy <public-judge@yandex.ru>
2023-02-11 12:22:18 +0300

    программа которая считает сумму чисел от 1 до а

---
+++ sem_4_1_sum/Program.cs
@@ -0,0 +1,12 @@
+﻿// программа которая считает сумму чисел от 1 до а
+Console.Clear();
+Console.WriteLine("Введите число ");
+int n= int.Parse (Console.ReadLine());
+int GetSum(int limit){
+    int sum =0;
+for (int i=1; i<limit; i++){
+    sum+=i;
+}
+return sum;
+}
+Console.WriteLine($"Сумма от 1 до {n} равно {GetSum(n)}");
